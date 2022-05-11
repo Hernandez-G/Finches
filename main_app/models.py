84f2dbe4f38aed4d957bdeaf1deeeec465e05b41
hernandez-g <g5hernandez@yahoo.com>
2022-05-11 12:35:18 -0700

    migration for toys

--- main_app/models.py
+++ main_app/models.py
@@ -1,11 +1,23 @@
 from django.db import models
 from django.urls import reverse
+from datetime import date
 
 MEALS = (
     ('B', 'Breakfast'),
     ('L', 'Lunch'),
     ('D', 'Dinner'),
 )
+
+class Toy(models.Model):
+  name = models.CharField(max_length=50)
+  color = models.CharField(max_length=20)
+
+  def __str__(self):
+    return self.name
+
+  def get_absolute_url(self):
+    return reverse('toys_detail', kwargs={'pk': self.id})
+
 
 class Finch(models.Model):
   name = models.CharField(max_length=100)
