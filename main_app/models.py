5edc35a787fce867c791cd32d5f57e1b7eeacb21
hernandez-g <g5hernandez@yahoo.com>
2022-05-11 11:06:34 -0700

    Part 4

--- main_app/models.py
+++ main_app/models.py
@@ -1,34 +1,40 @@
 from django.db import models
 from django.urls import reverse
 
-# Create your models here.
 MEALS = (
     ('B', 'Breakfast'),
     ('L', 'Lunch'),
-    ('D', 'Dinner')
+    ('D', 'Dinner'),
 )
 
-
 class Finch(models.Model):
-    name = models.CharField(max_length=100)
-    breed = models.CharField(max_length=100)
-    description = models.TextField(max_length=250)
-    age = models.IntegerField()
+  name = models.CharField(max_length=100)
+  breed = models.CharField(max_length=100)
+  description = models.TextField(max_length=250)
+  age = models.IntegerField()
 
-def __str__(self):
+  def __str__(self):
     return f'{self.name} ({self.id})'
 
-def get_absolute_url(self):
-    return reverse('detail', kwargs={'cat_id': self.id})
+  def get_absolute_url(self):
+    return reverse('detail', kwargs={'finch_id': self.id})
 
 class Feeding(models.Model):
-    date = models.DateField()
-    meal = models.CharField(
-        max_length=1,
-        choices=MEALS,
-        default=MEALS[0][0]
-        )
-finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+  date = models.DateField('Feeding Date')
+  meal = models.CharField(
+    max_length=1,
+    choices=MEALS,
+    default=MEALS[0][0]
+  )
 
-def __str__(self):
-    return f'{self.get_meal_display()} on {self.date}'
+  finch = models.ForeignKey(
+        Finch, 
+        on_delete=models.CASCADE
+    )
+
+  def __str__(self):
+    # Nice method for obtaining the friendly value of a Field.choice
+    return f"{self.get_meal_display()} on {self.date}"
+
+class Meta:
+    ordering = ['-date']
--- main_app/views.py
+++ main_app/views.py
@@ -1,33 +1,46 @@
-from django.shortcuts import render
-from django.views.generic.edit import CreateView,UpdateView, DeleteView
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Finch
-
+from .forms import FeedingForm
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+  return render(request, 'home.html')
 
 def about(request):
-    return render(request, 'about.html')
+  return render(request, 'about.html')
 
 def finches_index(request):
   finches = Finch.objects.all()
-  return render(request, 'finches/index.html', { 'finches': finches })
+  #                             data context |
+  return render(request, 'finches/index.html', {
+    'finches': finches,
+  })
 
 def finches_detail(request, finch_id):
   finch = Finch.objects.get(id=finch_id)
+  feeding_form = FeedingForm()
   return render(request, 'finches/detail.html', {
-    'finch': finch
+    'finch': finch,
+    'feeding_form': feeding_form
   })
 
 class FinchCreate(CreateView):
   model = Finch
   fields = '__all__'
-  success_url = '/finches/'
-
+  
 class FinchUpdate(UpdateView):
-  model = Finchfields = ['breed', 'description', 'age']
+  model = Finch
+  fields = ['breed', 'description', 'age']
 
 class FinchDelete(DeleteView):
   model = Finch
   success_url = '/finches/'
+
+def add_feeding(request, finch_id):
+  form = FeedingForm(request.POST)
+  if form.is_valid():
+    new_feeding = form.save(commit=False)
+    new_feeding.finch_id = finch_id
+    new_feeding.save()
+  return redirect('detail', finch_id=finch_id)
